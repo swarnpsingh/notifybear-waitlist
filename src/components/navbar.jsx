0f3274067bd28791d084ad219f1dc23d07135a39
Swarn Pratap Singh <swarnpratapsingh5@gmail.com>
2024-06-22 22:18:01 +0530

    positioning changes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,11 +11,11 @@ function Navbar() {
   return (
     <>
     <div className='w-full flex justify-between items-center'>
-        <div className='flex justify-center items-center gap-4'>
+        <div className='flex justify-center items-center gap-2 sm:gap-4'>
             <img src={icon} alt="" className='w-10'/>
             <h1 className='font-bold text-xl'>notifybear</h1>
         </div>
-        <div className='flex gap-6'>
+        <div className='flex sm:gap-6 gap-2'>
             <AiOutlineInstagram className='text-2xl sm:text-3xl cursor-pointer' />
             <AiOutlineTwitter className='text-2xl sm:text-3xl cursor-pointer' />
             <AiFillLinkedin className='text-2xl sm:text-3xl cursor-pointer' />
