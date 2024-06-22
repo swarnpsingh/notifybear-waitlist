428138bffd39c54d652113130231fc3bbc9448a9
Swarn Pratap Singh <swarnpratapsingh5@gmail.com>
2024-06-22 22:15:18 +0530

    changes in title, icon and navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,19 +5,21 @@ import {
     AiFillLinkedin,
     AiFillYoutube
 } from "react-icons/ai";
+import icon from '../assets/icon-mascot.png'
 
 function Navbar() {
   return (
     <>
     <div className='w-full flex justify-between items-center'>
-        <div>
+        <div className='flex justify-center items-center gap-4'>
+            <img src={icon} alt="" className='w-10'/>
             <h1 className='font-bold text-xl'>notifybear</h1>
         </div>
         <div className='flex gap-6'>
-            <AiOutlineInstagram className='text-3xl cursor-pointer' />
-            <AiOutlineTwitter className='text-3xl cursor-pointer' />
-            <AiFillLinkedin className='text-3xl cursor-pointer' />
-            <AiFillYoutube className='text-3xl cursor-pointer' />
+            <AiOutlineInstagram className='text-2xl sm:text-3xl cursor-pointer' />
+            <AiOutlineTwitter className='text-2xl sm:text-3xl cursor-pointer' />
+            <AiFillLinkedin className='text-2xl sm:text-3xl cursor-pointer' />
+            <AiFillYoutube className='text-2xl sm:text-3xl cursor-pointer' />
         </div>
     </div>
     </>
